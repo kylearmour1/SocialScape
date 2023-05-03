b2576aeef32744f32f0dfd3692cec51be9a3d691
kylearmour1 <kylejarmour@gmail.com>
2023-05-02 17:06:48 -0700

    get routes for though and user id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -11,4 +11,23 @@ const thoughtController = {
         console.log(err);
         res.status(400).json(err);
       });
-  }};
+  },
+  // get one thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .select('-__v')
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+
+};
