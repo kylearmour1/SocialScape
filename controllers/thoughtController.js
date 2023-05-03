3bf665b7c5a48aed999d1904fb194dfa5170309f
kylearmour1 <kylejarmour@gmail.com>
2023-05-02 19:06:14 -0700

    updating user and thought controllers by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -46,6 +46,21 @@ createThought({ body }, res) {
       res.json(dbUserData);
     })
     .catch((err) => res.status(400).json(err));
+},
+// update thought by id
+updateThought({ params, body }, res) {
+  Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought found with this id!' });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch((err) => res.status(400).json(err));
 }
 
+
 };
+
+module.exports = thoughtController;
--- controllers/userController.js
+++ controllers/userController.js
@@ -45,6 +45,21 @@ const userController = {
     User.create(body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(400).json(err));
+  },
+   // update user by id
+   updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
   }
 
+
 };
+
+module.exports = userController;
